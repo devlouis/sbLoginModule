becde7ac1a3223274843f234c8b5750486021146
Luis Lopez <prov284@franquiciasperu.com>
2022-06-15 22:26:40 -0500

    login create

---
+++ sblogin/src/main/java/com/starbucks/main/sblogin/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.starbucks.main.sblogin.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.starbucks.main.sblogin.data.LoginDataSource
+import com.starbucks.main.sblogin.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
